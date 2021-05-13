a0b55232e03693589a62f15283f49319fa746a06
LighthouseKeeperYN <freesaber@yandex.ru>
2021-05-13 20:00:49 +0300

    feat: add user routes

--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -4,8 +4,29 @@ const usersService = require('./user.service');
 
 router.route('/').get(async (req, res) => {
   const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
   res.json(users.map(User.toResponse));
+});
+
+router.route('/:userId').get(async (req, res) => {
+  const users = await usersService.getById(req.params.id);
+  res.json(users.map(User.toResponse));
+});
+
+
+router.route('/').post(async (req, res) => {
+  const user = await usersService.create(req.body);
+  res.json(User.toResponse(user));
+});
+
+
+router.route('/:userId').put(async (req, res) => {
+  const user = await usersService.update(req.params.id, req.body);
+  res.json(User.toResponse(user));
+});
+
+router.route('/:userId').delete(async (req, res) => {
+  const user = await usersService.remove(req.params.id);
+  res.json(User.toResponse(user));
 });
 
 module.exports = router;
