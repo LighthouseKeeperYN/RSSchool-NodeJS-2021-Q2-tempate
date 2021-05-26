d4290f929d7e9a5eba3f1c3d1cf8e617c839e970
LighthouseKeeperYN <freesaber@yandex.ru>
2021-05-27 01:28:59 +0300

    add more strict rules and comment out test entities

--- src/db.ts
+++ src/db.ts
@@ -1,46 +1,63 @@
-import Board from './resources/boards/board.model.js'
-import Column from './resources/columns/column.model.js'
-import Task from './resources/tasks/task.model.js'
-import User from './resources/users/user.model.js'
+import /* Board, */ { IBoard } from './resources/boards/board.model.js'
+import /* Column, */ { IColumn } from './resources/columns/column.model.js'
+import /* Task, */ { ITask } from './resources/tasks/task.model.js'
+import /* User, */ { IUser } from './resources/users/user.model.js'
 
-const testBoard = new Board({
-  id: 'testBoard',
-  title: 'testTitle',
-  columns: ['testColumn']
-})
+// const testBoard = new Board({
+//   id: 'testBoard',
+//   title: 'testTitle',
+//   columns: ['testColumn']
+// })
 
-const testUser = new User({
-  id: 'testUser',
-  name: 'testName',
-  login: 'testLogin',
-  password: 'testPassword',
-})
+// const testUser = new User({
+//   id: 'testUser',
+//   name: 'testName',
+//   login: 'testLogin',
+//   password: 'testPassword',
+// })
 
-const testColumn = new Column({
-  id: 'testColumn',
-  title: 'testTitle',
-  order: 0
-})
+// const testColumn = new Column({
+//   id: 'testColumn',
+//   title: 'testTitle',
+//   order: 0
+// })
 
-const testTask = new Task({
-  id: 'testTask',
-  title: 'testTitle',
-  order: 0,
-  description: 'testDescription',
-  userId: testUser.id,
-  boardId: testBoard.id,
-  columnId: testColumn.id,
-})
+// const testTask = new Task({
+//   id: 'testTask',
+//   title: 'testTitle',
+//   order: 0,
+//   description: 'testDescription',
+//   userId: testUser.id,
+//   boardId: testBoard.id,
+//   columnId: testColumn.id,
+// })
 
-const db = {
-  boards: { [testBoard.id]: testBoard },
-  columns: { [testColumn.id]: testColumn },
+interface IDb {
+  users: {
+    [userId: string]: IUser
+  },
+  boards: {
+    [boardId: string]: IBoard
+  },
+  columns: {
+    [columnID: string]: IColumn
+  },
   tasks: {
-    [testBoard.id]: {
-      [testTask.id]: testTask
+    [boardId: string]: {
+      [taskId: string]: ITask
     }
   },
-  users: { [testUser.id]: testUser }
+}
+
+const db: IDb = {
+  users: { /* [testUser.id]: testUser */ },
+  boards: { /* [testBoard.id]: testBoard */ },
+  columns: { /* [testColumn.id]: testColumn */ },
+  tasks: {
+/*     [testBoard.id]: {
+      [testTask.id]: testTask
+    } */
+  },
 }
 
 export default db
--- src/resources/tasks/task.memory.repository.ts
+++ src/resources/tasks/task.memory.repository.ts
@@ -16,8 +16,9 @@ export const create = async (boardId: string, task: ITask) => {
     throw new Error("Board doesn't exist")
   }
 
-  db.tasks[boardId][task.id] = task
-  return db.tasks[boardId][task.id]
+  db.tasks[boardId]![task.id] = task
+
+  return db.tasks[boardId]![task.id]
 };
 
 export const update = async (boardId: string, taskId: string, task: ITask) => {
@@ -25,8 +26,8 @@ export const update = async (boardId: string, taskId: string, task: ITask) => {
     throw new Error('Task not found')
   }
 
-  db.tasks[boardId][taskId] = { ...db.tasks[boardId][taskId], ...task }
-  return db.tasks[boardId][taskId]
+  db.tasks[boardId]![taskId] = { ...db.tasks[boardId]![taskId], ...task }
+  return db.tasks[boardId]![taskId]
 };
 
 export const remove = async (boardId: string, taskId: string) => {
@@ -34,8 +35,8 @@ export const remove = async (boardId: string, taskId: string) => {
     throw new Error('Task not found')
   }
 
-  const deletedTask = db.tasks[boardId][taskId]
-  delete db.tasks[boardId][taskId]
+  const deletedTask = db.tasks[boardId]![taskId]
+  delete db.tasks[boardId]![taskId]
   return deletedTask
 }
 
@@ -43,6 +44,6 @@ export const unassignAll = async (userId: string) => {
   const allTasks = Object.values(db.tasks).map(boardTasks => Object.values(boardTasks)).flat()
   allTasks.forEach((task: ITask) => {
     if (!(task.userId === userId) || !db.tasks[task.boardId]) return
-    db.tasks[task.boardId][task.id].userId = null
+    db.tasks[task.boardId]![task.id]!.userId = null
   })
 }
